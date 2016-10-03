ad08e6d42e8d324a91574a7765c7883941910ccf
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 18:09:11 -0700

    GET w/ access token

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function loadData(){
   jQuery.ajax( {
-      url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9',
+      url: 'https://api.instagram.com/v1/users/25025320/media/recent/?client_id=f574fddc748b46e4ba3db72787691687&callback=?',
       type: 'GET',
-      data: { link: '' },
+      data: data,
+      beforeSend : function( xhr ) {
+        xhr.setRequestHeader('access_token' + '1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9' );
+      },
       success: function( response ) {
         console.log(success);
-          // response
       }
   } );
 }
@@ -13,6 +15,8 @@ function loadData(){
 function loadJSONcall() {
   $.getJSON(
         'https://api.instagram.com/v1/users/25025320/media/recent/?client_id=f574fddc748b46e4ba3db72787691687&callback=?',
+
+
         'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9',
         function(data) {
             console.log(data);
