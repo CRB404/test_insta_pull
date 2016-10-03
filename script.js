e941428b7665271889d6292a56cc3813435f29d4
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 18:24:12 -0700

    undo array

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function loadJSONcall() {
   $.getJSON(
         'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9&callback=?',
         function(data) {
-            console.log(data[0]);
+            console.log(data);
         }
     );
 
