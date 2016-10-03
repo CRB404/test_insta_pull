2c155326b89814b3661b42f0783b35378c6407db
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 18:19:56 -0700

    implicit is active

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 function loadData(){
   jQuery.ajax( {
-      url: 'https://api.instagram.com/v1/users/25025320/media/recent/?client_id=f574fddc748b46e4ba3db72787691687&callback=?',
+      url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9',
       type: 'GET',
-      data: data,
-      beforeSend : function( xhr ) {
-        xhr.setRequestHeader('access_token' + '1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9' );
-      },
+      data: { link: '' },
       success: function( response ) {
         console.log(success);
+          // response
       }
   } );
 }
 
 function loadJSONcall() {
   $.getJSON(
-        'https://api.instagram.com/v1/users/25025320/media/recent/?client_id=f574fddc748b46e4ba3db72787691687&callback=?',
-        function( xhr ) {
-          xhr.setRequestHeader('access_token' + '1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9' );
-        },
+        'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9',
         function(data) {
             console.log(data);
         }
