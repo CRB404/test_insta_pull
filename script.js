41dd70903a33fa2b140946cf748c2acdc21490b3
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 19:11:19 -0700

    console array

--- script.js
+++ script.js
@@ -5,21 +5,16 @@ function loadJSONcall() {
           var instaData = data;
           console.log(instaData);
           console.log("instaData ________________________");
-          var lastImg = data.data.slice(0);
-          console.log(lastImg);
+          var Images = data.data.slice(0);
+          console.log(Images);
           console.log("lastImg ________________________");
 
 
           for (var i=0; i<6; i++)
-            for (var name in lastImg[i]) {
-                console.log("0: "+name);
-                console.log("1: "+lastImg[i][name]);
-                console.log("2: "+lastImg[i][name]);
-                console.log("3: "+lastImg[i][name]);
-                console.log("4: "+lastImg[i][name]);
-                console.log("5: "+lastImg[i][name]);
+            for (var name in Images[i]) {
+                console.log("parent: "+name);
+                console.log("child: "+lastImg[i][name]);
 
-                console.log("6: "+lastImg[6][name]);
             }
 
 
