62d461d357da2af9df40223b62510a94d963f2ab
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 17:34:42 -0700

    CORS fix on access control

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function loadData(){
   jQuery.ajax( {
-      url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9&callback=?',
+      Access-Control-Allow-Origin: https: 'https://crb404.github.io/test_insta_pull/',
+      url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9',
       type: 'GET',
       data: { link: '' },
       success: function( response ) {
