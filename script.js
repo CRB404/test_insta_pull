c07388d3fcf339ace4919db2efe84a7e394b617a
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 17:43:56 -0700

    correcting errors

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function loadData(){
   jQuery.ajax( {
-      Access-Control-Allow-Origin: https: 'https://crb404.github.io/test_insta_pull/',
       url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9',
       type: 'GET',
       data: { link: '' },
