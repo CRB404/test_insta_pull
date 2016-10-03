3a65bf3eec8d227917c91c36209769eec129d4ae
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 18:32:36 -0700

    finding array

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function loadJSONcall() {
   $.getJSON(
         'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9&callback=?',
-        function(data.data) {
-            console.log(data.data);
+        function(data) {
+          var instaData = data;
+          console.log(instaData);
+          var sliceData = instaData.slice(0);
+          console.log("________________________");
+          console.log(instaData);
         }
     );
 
