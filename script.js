a8cc42849e8e7ba1975a26a777e79c783c4dfe27
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 17:30:43 -0700

    add access token

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function loadData(){
-  console.log("test")
   jQuery.ajax( {
-      url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=ACCESS-TOKEN',
+      url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9&callback=?',
       type: 'GET',
       data: { link: '' },
       success: function( response ) {
