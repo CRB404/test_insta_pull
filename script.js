8adff629ef5e8c3f2bd9a6aaa92fb8ecfa7991b2
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 18:20:47 -0700

    with callback

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function loadData(){
 
 function loadJSONcall() {
   $.getJSON(
-        'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9',
+        'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9&callback=?',
         function(data) {
             console.log(data);
         }
