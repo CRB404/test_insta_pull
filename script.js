3b12e02af6f9ad54b28aa3e336ee8fea83dd38ff
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 18:11:05 -0700

    correct function syntax

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function loadData(){
 function loadJSONcall() {
   $.getJSON(
         'https://api.instagram.com/v1/users/25025320/media/recent/?client_id=f574fddc748b46e4ba3db72787691687&callback=?',
-        beforeSend : function( xhr ) {
+        function() {
           xhr.setRequestHeader('access_token' + '1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9' );
         },
         function(data) {
