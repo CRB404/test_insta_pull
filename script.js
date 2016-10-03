791c9d35c02bfe9a351dce407b453f746cc893c2
CRB404 <adamlukasik@Adams-MacBook-Pro-2.local>
2016-10-02 18:30:02 -0700

    pull array

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function loadJSONcall() {
   $.getJSON(
         'https://api.instagram.com/v1/users/self/media/recent/?access_token=1377128560.1677ed0.3cbc51058d3448be81bf01626ad230e9&callback=?',
-        function(data) {
-            console.log(data);
+        function(data.data) {
+            console.log(data.data);
         }
     );
 
